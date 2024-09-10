ff3e1fcc2f3364932069fcdbb922bdaeb078af4b
maxihac <cmaxihac@gmail.com>
2024-09-10 01:21:50 -0300

    Reparando codigo2

---
+++ src/main/java/edu/coderhouse/FacturacionPrimeraEntrega/service/VentaService.java
@@ -0,0 +1,57 @@
+package edu.coderhouse.FacturacionPrimeraEntrega.service;
+
+import edu.coderhouse.FacturacionPrimeraEntrega.model.Producto;
+import edu.coderhouse.FacturacionPrimeraEntrega.model.Venta;
+import edu.coderhouse.FacturacionPrimeraEntrega.model.VentaProducto;
+import edu.coderhouse.FacturacionPrimeraEntrega.repository.VentaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VentaService {
+    @Autowired
+    private VentaRepository ventaRepository;
+
+    public Venta agregarVenta(Venta venta) {
+        try {
+            // Establecer la relación entre Venta y VentaProducto
+            for (VentaProducto vp : venta.getVentaProductos()) {
+                vp.setVenta(venta);
+            }
+            return ventaRepository.save(venta);
+        } catch (Exception e) {
+            // Registrar el error
+            System.err.println("Error al guardar la venta: " + e.getMessage());
+            throw new RuntimeException("Error al guardar la venta", e);
+        }
+    }
+
+
+    public List<Venta> obtenerTodosLasVentas() {
+        return ventaRepository.findAll();
+    }
+
+    public Venta buscarVenta(Long id) {
+        return ventaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Venta no encontrado con ID: " + id));
+    }
+
+
+    public void borrarVenta(Long id) {
+        Venta venta = ventaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Venta no encontrada"));
+        ventaRepository.delete(venta);
+    }
+
+    public Venta actualizarVenta(Long id, Venta ventaActualizado) {
+        Venta ventaExistente = ventaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Venta no encontrada"));
+        ventaExistente.setTipofactura(ventaActualizado.getTipofactura());
+        ventaExistente.setFactura(ventaActualizado.getFactura());
+        ventaExistente.setFecha(ventaActualizado.getFecha());
+        ventaExistente.setTotal(ventaActualizado.getTotal());
+        return ventaRepository.save(ventaExistente);
+    }
+}
